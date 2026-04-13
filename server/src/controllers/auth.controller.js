59b139db749bb232dff4632c34d70ef6e87ef21b
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-13 14:04:08 -0700

    Create auth.controller.js

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,50 @@
+import User from '../models/User.js';
+import { generateToken } from '../utils/generateToken.js';
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Create user
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check for user email and explicitly select the password field (since it's hidden by default)
+    const user = await User.findOne({ email }).select('+password');
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
