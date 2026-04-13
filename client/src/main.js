322e86d759324b55b473ff9e5145cebec64cf250
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-13 13:53:56 -0700

    Create main.js

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+import './assets/global.css'; // Your global stylesheet
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
